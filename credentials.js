18f02d2525681ac133068c4d130ab72a4b319f8e
justinchiu <chiujiachengssvs@gmail.com>
2025-11-11 21:07:20 +0800

    測試部屬中

---
+++ credentials.js
@@ -0,0 +1,14 @@
+// 這是儲存所有帳號密碼與軍階的 JavaScript 物件。
+// 鍵 (key) 是您的 ROBLOX NAME (請區分大小寫)。
+// 值 (value) 是一個包含 'discordName' (請全部使用小寫) 和 'rank' (軍階) 的物件。
+const USER_CREDENTIALS = {
+    // 範例：請修改 "少將" 為該帳號的正確軍階
+    "Justin_chiu27": {
+        Password: "Sony9487",
+        Postion: "副指揮官" ,
+    }, 
+    
+    // 您可以在此處手動添加更多帳號
+  
+    
+};
