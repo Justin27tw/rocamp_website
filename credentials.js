f09318519b60fb04ba96b35818a37032eac42abe
justinchiu <chiujiachengssvs@gmail.com>
2025-11-13 11:46:21 +0800

    登入功能ok

--- credentials.js
+++ credentials.js
@@ -1,14 +1,174 @@
 // 這是儲存所有帳號密碼與軍階的 JavaScript 物件。
 // 鍵 (key) 是您的 ROBLOX NAME (請區分大小寫)。
-// 值 (value) 是一個包含 'discordName' (請全部使用小寫) 和 'rank' (軍階) 的物件。
-const USER_CREDENTIALS = {
-    // 範例：請修改 "少將" 為該帳號的正確軍階
+// 值 (value) 是一個包含 'discordname' (請全部使用小寫) 和 'Postion' (職位/軍階) 的物件。
+USER_CREDENTIALS = {
+    // 範例：ROBLOX NAME (區分大小寫)
     "Justin_chiu27": {
-        Password: "Sony9487",
-        Postion: "副指揮官" ,
+        discordname: "chiu_jia", // 密碼 (全部小寫)
+        Postion: "副指揮官" ,     // 職位/軍階
     }, 
     
-    // 您可以在此處手動添加更多帳號
-  
+    // 新增一個測試帳號
+    "sjkhortj": {
+        discordname: "ryan610",
+        Postion: "指揮官", 
+    },
+    "rockroll901": {
+        discordname: "rock_8964",
+        Postion: "參謀長", 
+    },
+    "zhuy1126": {
+        discordname: "zhuy1126",
+        Postion: "督導", 
+    },
     
+    "XxChortleKidd": {
+        discordname: "lwlhon_.0957",
+        Postion: "督導", 
+    },
+    "jianhao0909": {
+        discordname: "haohaosam",
+        Postion: "行政人員", 
+    },
+    
+    "nollihj": {
+        discordname: "qaq_128920",
+        Postion: "勤務大隊長", 
+    },
+    "aplkjjsffh": {
+        discordname: "doughuser0",
+        Postion: "勤務中隊長", 
+    },
+    
+    "yaya_yen": {
+        discordname: "yayay0253",
+        Postion: "特勤大隊長", 
+    },
+    
+    "Leosergo": {
+        discordname: "maolixiaowuguo",
+        Postion: "特勤中隊長", 
+    },
+    
+    "dammy526": {
+        discordname: "dammy526.",
+        Postion: "特勤幹部", 
+    },
+    "wayne11087": {
+        discordname: "wayne_11087",
+        Postion: "實習幹部", 
+    },
+    "hgjk66ll": {
+        discordname: "hgjk66ll",
+        Postion: "勤務幹部", 
+    },
+    "xtrey_yy1": {
+        discordname: "james_99733",
+        Postion: "榮譽憲兵", 
+    },
+    "Leokuo1210": {
+        discordname: "cookice",
+        Postion: "技術人員", 
+    },
+    "jack970411": {
+        discordname: "firstdreamchmng",
+        Postion: "國防部長", 
+    },
+    
+    "fhfhfhrip": {
+        discordname: "fhfhfhrip",
+        Postion: "國防副部長", 
+    },
+    "Zahue": {
+        discordname: ".a_han",
+        Postion: "常務次長", 
+    },
+    
+    "JackHuang1020": {
+        discordname: "adolf_1020",
+        Postion: "參謀總長", 
+    },
+    "Joee0110": {
+        discordname: "eeoj",
+        Postion: "副參謀總長", 
+    },
+    "kkchan0119": {
+        discordname: "cdl0119",
+        Postion: "工作總監", 
+    },
+    "rtuhjhhijh": {
+        discordname: "tw102567",
+        Postion: "檢察長", 
+    },
+    
+    "eiuegwgu": {
+        discordname: "eiuegwgu",
+        Postion: "勤務成員", 
+    },
+    "AOOOHOOOA": {
+        discordname: "hanguo0336",
+        Postion: "勤務成員", 
+    },
+    "Florentino_2312": {
+        discordname: "zruic7_",
+        Postion: "勤務成員", 
+    },
+    "hohim64": {
+        discordname: "him06390",
+        Postion: "勤務監察官", 
+    },
+    "D1ndolf": {
+        discordname: "dindolf_san",
+        Postion: "勤務成員", 
+    },
+    "Rain1011103": {
+        discordname: "guojunrain_00947",
+        Postion: "勤務成員", 
+    },
+    "fighter123456789": {
+        discordname: "fighter_1234789",
+        Postion: "勤務成員", 
+    },
+    "lgu9u9s1x1234": {
+        discordname: "yongjin5431",
+        Postion: "勤務成員", 
+    },
+    "djdkdhdek": {
+        discordname: "hki0733_16045",
+        Postion: "勤務成員", 
+    },
+    "TW_cia05": {
+        discordname: "tw_cia05",
+        Postion: "勤務成員", 
+    },
+    "Htbridisj": {
+        discordname: "xhh08168",
+        Postion: "勤務成員", 
+    },
+    "HGD_kemd": {
+        discordname: "hgd_kemd55",
+        Postion: "特勤成員", 
+    },
+    "WalkTheEarthMyself": {
+        discordname: "4orgotten64",
+        Postion: "特勤成員", 
+    },
+    "XZPSCA": {
+        discordname: "xzpsca",
+        Postion: "特勤成員", 
+    },
+    "ryan00606": {
+        discordname: "limo_60",
+        Postion: "特勤監察官", 
+    },
+    "Nck552": {
+        discordname: "mazu5606",
+        Postion: "特勤成員", 
+    },
+    "dsaidwiki7487": {
+        discordname: "dincuret",
+        Postion: "憲兵學員", 
+    },
+    
+    // 您可以在此處手動添加更多帳號
 };
